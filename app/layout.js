fa02b5af3b913e01fe445b9eed8999dc96782faa
Andrey Kondalov <andrey@kondalov.com>
2023-04-20 01:55:03 +0300

    refactor: remove unused components

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import Link from 'next/link';
-import Sidebar from '../components/Sidebar';
 
 function RootLayout({ children, extension }) {
   return (
     <html lang="en">
       <body style={{ display: 'flex' }}>
         <aside>
-          <Sidebar extension={extension} />
+          <div style={{ border: '1px solid red', height: 400, minWidth: 100 }}>
+            <h3>Sidebar</h3>
+            <div style={{ border: '1px solid blue' }}>{extension}</div>
+          </div>
         </aside>
         <div>
           <div>
